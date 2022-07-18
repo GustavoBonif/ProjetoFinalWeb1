809866a3ec12c0e41b0c2da11ac58fbc4bece873
GustavoBonif <gubonic@gmail.com>
2022-07-18 19:38:57 -0300

    Criando projeto cliente

---
+++ proj-cliente/src/views/CatalogView.vue
@@ -0,0 +1,81 @@
+<script>
+export default {
+    methods: {
+        ...mapActions(useCatalogStore, [
+        "getCatalog",
+        // "saveBook",
+        // "deleteBook",
+        ]),
+        ...mapActions(useCatalogStore, ["getCatalog"]),
+        // ...mapActions(useAuthorStore, ["getAllAuthors"]),
+        // ...mapActions(usePublisherStore, ["getAllPublishers"]),
+        // async save() {
+        //     try {
+        //         const msg = await this.saveBook(this.currentBook);
+        //         alert(msg);
+        //         this.editing = false;
+        //         this.currentBook = {};
+        //     } catch (e) {
+        //         alert("Ooops! Algo de errado!");
+        //     }
+        // },
+        // async deleteItem(book_id) {
+        //     try {
+        //         await this.deleteBook(book_id);
+        //         alert("Item excluído com sucesso.");
+        //     } catch (e) {
+        //         alert(e);
+        //     }
+        // },
+        prepareToUpdate(book) {
+        Object.assign(this.currentBook, book);
+        this.editing = true;
+        },
+    },
+}
+</script>
+
+<template>
+    <section class="container">
+        <div class="item">
+            <div class="book">
+
+            </div>
+            <div class="description">
+                <h3>Os miseraveis</h3>
+                <h5>Autor: Victor Hugo</h5>
+                <h5>Gênero: Victor Hugo</h5>
+            </div>
+        </div>
+        <div class="item">
+            <div class="book">
+
+            </div>
+            <div class="description">
+                <h3>Os miseraveis</h3>
+                <h5>Autor: Victor Hugo</h5>
+                <h5>Gênero: Victor Hugo</h5>
+            </div>
+        </div>
+        <div class="item">
+            <div class="book">
+
+            </div>
+            <div class="description">
+                <h3>Os miseraveis</h3>
+                <h5>Autor: Victor Hugo</h5>
+                <h5>Gênero: Victor Hugo</h5>
+            </div>
+        </div>
+        <div class="item">
+            <div class="book">
+
+            </div>
+            <div class="description">
+                <h3>Os miseraveis</h3>
+                <h5>Autor: Victor Hugo</h5>
+                <h5>Gênero: Victor Hugo</h5>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ proj-cliente/src/views/HomeView.vue
@@ -0,0 +1,97 @@
+<script setup></script>
+
+<template>
+  <h1>Sistema de gestão de produtos</h1>
+  <h2>Fazer uma landpage</h2>
+</template>
+
+<style>
+    body{
+        background-color: #d9d9d9;
+        margin: 0;
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #373337;
+    }
+
+    li {
+        float: left;
+    }
+
+    li h1 {
+        display: block;
+        text-align: center;
+        padding: 0.5vh;
+        text-decoration: none;
+        color: white;
+    }
+
+    #carrinho{
+        float: right;
+        text-align: center;
+        padding: 0.5vh;
+        text-decoration: none;
+        color: white;
+    }
+
+    .container{
+        margin: auto;
+        width: 95%;
+        height: 65vh;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .item{
+        margin: 25px;
+        background-color: #868585;
+        text-align: center;
+        font-size: 1.5em;
+        width: 46.5%;
+        align-items: center;
+        text-align: left;
+    }   
+
+    .container_search{
+        margin: auto;
+        height: 20vh;
+        display: flex;
+       
+    }
+
+    .search{
+        width: 50%;
+        height: 40%;
+        margin: auto;
+        text-align: center;
+    }
+
+    .search_bar{
+        width: 60%;
+        height: 60%;
+        margin: auto;
+        align-items: center;
+        align-content: center;
+        text-align: center;
+        border-style: none;
+        border-radius: 25px;
+    }
+
+    .book{
+        background-color: #373337;
+        float: left;
+        width: 30%;
+        height: 100%;
+    }
+    
+    .description{
+        width: 65%;
+        float: right;
+        margin-top: 0;
+    }
+</style>
